8f749bbb6ec7a82f4a39e7f01bfa701ea48153e3
오미선 <momo.class5@gmail.com>
2024-09-10 16:49:30 +0900

    오류처리

--- ex/ex10.calc.py
+++ ex/ex10.calc.py
@@ -39,7 +39,7 @@ def getGender(ssn) :
     # 유효성검증
     ssn = str(ssn)
     ssn = ssn.replace('-', '')
-    if not ssn.isnumeric :
+    if not ssn.isnumeric() :
         print('숫자만 입력 가능 합니다.')
         return
     if len(ssn) != 13 :
@@ -86,42 +86,59 @@ print('='*20, 'getInfo()')
 def getInfo(name, ssn) : 
     # 이름, 나이, 주민번호를 반환
     name = name
+    # 함수를 호출 하여 나이와 성별을 반환 받아 변수에 저장
     age = getAge(ssn)
-    print(age)
+    gender = getGender(ssn)
     
-    info = [name, age]
+    info = [name, age, gender]
     return info
     
-getInfo('박서현', '1111113333333')
+a, b, c = getInfo('박서현', '1111113333333')
+print(f'이름 : {a}, 나이 : {b}, 성별 : {c}')
 
 # 이름과, 주민등록번호를 입력받아서 이름, 성별, 나이를 반환하는 함수
 # getGender()
 print("=" *20)
 
 
-
-
-# 함수를 만들어 봅시다
-# 함수명 : calc(operator, *args)
-#   operator : 연산자
-#   args : 숫자가 저장된 튜플
-def calc(*args) :
-    res = 0
-    # *을 사용하면 튜플로 값을 받아온다
-    # 매개변수를 넣지 않으면 빈 튜플로 처리된다
-    print(args)
-    # args의 요수의 갯수는 2개 이상이여야 한다 
-    print('len(args) : ', len(args))
-    if len(args) < 2 :
-        print('수를 2개 이상 입력 해주세요')
-        return res
-    
-    # 튜플에 있는 요소들의 합계를 구하는 반복문
-    for num in args : 
-        res += num
+# 계산기 - 입력이 잘못된경우 0을 반환
+def cacl(operator, num1, num2) :
     # operator : +-*/%
-    return res
+    if operator not in ['+', '-', '*', '/', '%'] :
+        return 0
+    # .isnumeric() : 문자열이 가지고 있는 함수
+    # 문자열인 경우에만 사용이 가능 
+    # 타입이 다른 경우 사용 할 수 없음
+    #   AttributeError: 'int' object has no attribute 'isnumeric'
+    # 함수에 괄호를 붙이지 않았을 때
+    #   <built-in method isnumeric of str object at 0x0000026C06345050>
+    print('================', str(num1).isnumeric() )
+    if operator == '+' : 
+        return num1 + num2
+    elif operator == '-' : 
+        return num1 - num2
+    elif operator == '*' : 
+        return num1 * num2
+    elif operator == '/' : 
+        return num1 / num2
+    elif operator == '%' : 
+        return num1 % num2
+    
+print('calc(+,10,100)', cacl('+', 10, 100))
 
-# 매개변수를 넣지 않아도 오류가 발생 되지 않는다
-print('calc() : ', calc())
-print('calc() : ', calc(1,2))
+# 평균을 구해 봅시다
+def avg(*numbers) :
+    print('numbers', numbers)
+    #return sum(numbers)/len(numbers)
+    sum = 0
+    for num in numbers : 
+        sum +=num
+    return sum/len(numbers)
+print('avg : ', avg(1,2,3,4,5,6,7,8,9,10))
+
+
+# 리스트, 튜플, 딕셔너리, set
+# bool()
+# datetime
+# random
+# 함수(def, 매개변수, 반환)
